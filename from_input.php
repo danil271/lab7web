b22401f534d04c3889929f8931088d31ca55def3
danil271 <81598231+danil271@users.noreply.github.com>
2021-05-10 00:03:45 +0700

    Create from_input.php

---
+++ from_input.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab7WEB by: Danil Bahri Tanjung</title>
+</head>
+<body style="">
+    <h1>Lab7WEB by: Danil Bahri Tanjung</h1>
+    <form style="" action="database.php" method="post">
+        Nama: 
+        <input type="text" name="nama" id="nama">
+        <br>
+        <br>
+        tanggal lahir: 
+        <input type="date" name="tgl_lahir" id="tgl_lahir">
+        <br>
+        <br>
+        Pekerjaan: 
+        <select name="pekerjaan" id="pekerjaan">
+            <option value="wirasuasta">wirasuasta</option>
+            <option value="supir">supir</option>
+            <option value="pengacara">pengacara</option>
+            <option value="chef">chef</option>
+        </select>
+        <br>
+        <br>
+        <input type="submit" value="submit" name="submit">
+    </form>
+</body>
+</html>
